1d16dc9d9a66cb6a341792de4d5efce2dd4fd92d
StanislavTsyganko <sts035@mail.ru>
2025-07-11 17:37:57 +0300

    edit_main_settings

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
+# from models import Citata
 
 def main(request):
     return render(request, 'main.html')
 
-def add(request):
-    return render(request, 'add.html')
-
 def top(request):
     return render(request, 'top.html')
+
