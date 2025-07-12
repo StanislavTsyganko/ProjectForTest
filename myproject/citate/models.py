bbfa9b1be9ecda06c70661509cb4abf709947451
StanislavTsyganko <sts035@mail.ru>
2025-07-12 16:28:27 +0300

    change_db

--- myproject/citate/models.py
+++ myproject/citate/models.py
@@ -5,8 +5,9 @@ class Citata(models.Model):
     content = models.TextField(unique=True)
     character = models.CharField(max_length=75)
     date = models.DateTimeField(auto_now_add=True)
-    weight = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(1)])
+    weight = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(1)], default=0)
     views = models.IntegerField(default=0)
+    raiting = models.IntegerField(default=0)
 
     def __str__(self):
         return self.content
--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render
 from citate.models import Citata
 
@@ -5,11 +6,17 @@ def find_random_citate(citates):
     score = 0
     for citate in citates:
         score+=citate.weight
-    math.rand()
+    index = random.randint(0,score)
+    for citate in citates:
+        if score-citate.weight>0:
+            score-=citate
+        else:
+            return citate
 
 def main(request):
     citates = Citata.objects.all()
-    return render(request, 'main.html', {'citates':citates})
+    citate = find_random_citate(citates)
+    return render(request, 'main.html', {'citate':citate})
 
 def top(request):
     return render(request, 'top.html')
