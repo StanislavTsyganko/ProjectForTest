1e91f94fb78b8ddd802816c5df6941ba7ba95b59
StanislavTsyganko <sts035@mail.ru>
2025-07-14 12:44:55 +0300

    change_db

--- myproject/citate/forms.py
+++ myproject/citate/forms.py
@@ -4,7 +4,7 @@ from . import models
 class CreateCitate(forms.ModelForm):
     class Meta:
         model = models.Citata
-        fields = ['content', 'character', 'weight']
-        labels = {"content":"Цитата", "character": "Персонаж или автор", "weight":"Вес"}
+        fields = ['content', 'source', 'character', 'weight']
+        labels = {"content":"Цитата", "source":"Источник", "character": "Персонаж или автор", "weight":"Вес"}
 
 # class EditCitate(forms.ModelForm):
